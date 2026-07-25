f90164e5e6e7c0125e08e5a40b72c547cb1cc6c0
Pavel Korolev <korolev.vrn@gmail.com>
2026-07-25 23:31:30 +0300

    test: fix subordinate register validation fixture

--- crates/unica-coder/src/infrastructure/native_operations/meta.rs
+++ crates/unica-coder/src/infrastructure/native_operations/meta.rs
@@ -5411,14 +5411,45 @@ mod edit_tests {
         write_mode: &str,
         use_standard_commands: &str,
     ) -> String {
+        let internal_info = r#"		<InternalInfo>
+			<xr:GeneratedType name="InformationRegisterRecord.SampleRegister" category="Record">
+				<xr:TypeId>11111111-1111-4111-8111-111111111111</xr:TypeId>
+				<xr:ValueId>21111111-1111-4111-8111-111111111111</xr:ValueId>
+			</xr:GeneratedType>
+			<xr:GeneratedType name="InformationRegisterManager.SampleRegister" category="Manager">
+				<xr:TypeId>12222222-2222-4222-8222-222222222222</xr:TypeId>
+				<xr:ValueId>22222222-2222-4222-8222-222222222222</xr:ValueId>
+			</xr:GeneratedType>
+			<xr:GeneratedType name="InformationRegisterSelection.SampleRegister" category="Selection">
+				<xr:TypeId>13333333-3333-4333-8333-333333333333</xr:TypeId>
+				<xr:ValueId>23333333-3333-4333-8333-333333333333</xr:ValueId>
+			</xr:GeneratedType>
+			<xr:GeneratedType name="InformationRegisterList.SampleRegister" category="List">
+				<xr:TypeId>14444444-4444-4444-8444-444444444444</xr:TypeId>
+				<xr:ValueId>24444444-4444-4444-8444-444444444444</xr:ValueId>
+			</xr:GeneratedType>
+			<xr:GeneratedType name="InformationRegisterRecordSet.SampleRegister" category="RecordSet">
+				<xr:TypeId>15555555-5555-4555-8555-555555555555</xr:TypeId>
+				<xr:ValueId>25555555-5555-4555-8555-555555555555</xr:ValueId>
+			</xr:GeneratedType>
+			<xr:GeneratedType name="InformationRegisterRecordKey.SampleRegister" category="RecordKey">
+				<xr:TypeId>16666666-6666-4666-8666-666666666666</xr:TypeId>
+				<xr:ValueId>26666666-6666-4666-8666-666666666666</xr:ValueId>
+			</xr:GeneratedType>
+			<xr:GeneratedType name="InformationRegisterRecordManager.SampleRegister" category="RecordManager">
+				<xr:TypeId>17777777-7777-4777-8777-777777777777</xr:TypeId>
+				<xr:ValueId>27777777-7777-4777-8777-777777777777</xr:ValueId>
+			</xr:GeneratedType>
+		</InternalInfo>"#;
         let xml = sample_meta_object_xml(
             "InformationRegister",
             "SampleRegister",
             &format!(
                 "\t\t\t<UseStandardCommands>{use_standard_commands}</UseStandardCommands>\n\t\t\t<WriteMode>{write_mode}</WriteMode>"
             ),
-            "\t\t<ChildObjects>\n\t\t\t<Dimension uuid=\"66666666-6666-4666-8666-666666666666\">\n\t\t\t\t<Properties>\n\t\t\t\t\t<Name>SampleDimension</Name>\n\t\t\t\t</Properties>\n\t\t\t</Dimension>\n\t\t</ChildObjects>",
-        );
+            "\t\t<ChildObjects>\n\t\t\t<Dimension uuid=\"66666666-6666-4666-8666-666666666666\">\n\t\t\t\t<Properties>\n\t\t\t\t\t<Name>SampleDimension</Name>\n\t\t\t\t\t<Type><v8:Type>xs:string</v8:Type></Type>\n\t\t\t\t</Properties>\n\t\t\t</Dimension>\n\t\t</ChildObjects>",
+        )
+        .replace("\t\t<Properties>", &format!("{internal_info}\n\t\t<Properties>"));
         let outcome = validate_registered_object(
             "InformationRegister",
             "SampleRegister",
